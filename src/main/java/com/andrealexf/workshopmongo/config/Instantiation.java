6b7eb08ebd3c0b04c12eb84c3262fdc96fd048b9
Andre Fukuda <andre.alex.fukuda@gmail.com>
2024-09-02 14:28:49 -0300

    Operação de instanciação da base de dados

---
+++ src/main/java/com/andrealexf/workshopmongo/config/Instantiation.java
@@ -0,0 +1,31 @@
+package com.andrealexf.workshopmongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.andrealexf.workshopmongo.domain.User;
+import com.andrealexf.workshopmongo.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner{
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		userRepository.deleteAll();//limpa a coleção 
+
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+	}
+
+	
+}
