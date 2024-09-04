e35bd94eacd47dbeb257c361d80c57e9750229cd
Andre Fukuda <andre.alex.fukuda@gmail.com>
2024-09-04 15:28:27 -0300

    Consulta com vários critérios (query)

--- src/main/java/com/andrealexf/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/andrealexf/workshopmongo/repository/PostRepository.java
@@ -1,6 +1,7 @@
 package com.andrealexf.workshopmongo.repository;
 
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -12,9 +13,18 @@ import com.andrealexf.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 	
-	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")//0 é o primeiro parâmetro do método, 'i' ignora maiusculas e min
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")//se o texto perntence ao título
+	//0 é o primeiro parâmetro do método, 'i' ignora maiusculas e min
 	List<Post> searchTitle(String text);
 
 	List<Post> findByTitleContainingIgnoreCase(String text);//ignoreCase ignora maiusculas e minusculas
 	
+	
+	@Query("{ $and: [ { date: {$gte: ?1} }, { date: { $lte: ?2} } , { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")//comments.text = dentro texto de cada comentário
+	// $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } 
+	//or -> texto no título ou no corpo
+	//and -> data entre minima e máxima
+	//gte -> greater or equal 
+	//lte -> less or equal
+	List<Post> fullSearch(String text, Date minDate, Date maxDate); //txt = 0, minDat = 1, maxDat = 2
 }
--- src/main/java/com/andrealexf/workshopmongo/resource/util/URL.java
+++ src/main/java/com/andrealexf/workshopmongo/resource/util/URL.java
@@ -2,6 +2,10 @@ package com.andrealexf.workshopmongo.resource.util;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
 public class URL {
 
@@ -15,5 +19,19 @@ public class URL {
 			
 			return "";//ou retorna o string decodificado, ou um string vazio se der problema
 		}
+	}
+	
+	public static Date convertDate(String textDate, Date defaultValue) {
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		
+		try {
+			return sdf.parse(textDate);
+		} catch (ParseException e) {
+
+			return defaultValue;
+		}
+		
 	}
 }
