9f707072ba5decb2037d2e020a118a13182a0682
Andre Fukuda <andre.alex.fukuda@gmail.com>
2024-09-04 10:08:37 -0300

    Consulta simples com query methods

---
+++ src/main/java/com/andrealexf/workshopmongo/resource/util/URL.java
@@ -0,0 +1,19 @@
+package com.andrealexf.workshopmongo.resource.util;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+
+public class URL {
+
+	public static String decodeParam(String text) {
+		
+		try {
+			
+			return URLDecoder.decode(text, "UTF-8");
+			
+		} catch (UnsupportedEncodingException e) {
+			
+			return "";//ou retorna o string decodificado, ou um string vazio se der problema
+		}
+	}
+}
