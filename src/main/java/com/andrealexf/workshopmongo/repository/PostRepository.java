0f081396c8165e05b63475aa49ea33a9f73b1d29
Andre Fukuda <andre.alex.fukuda@gmail.com>
2024-09-04 14:38:47 -0300

    Consulta simples com query

--- src/main/java/com/andrealexf/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/andrealexf/workshopmongo/repository/PostRepository.java
@@ -4,12 +4,16 @@ package com.andrealexf.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.andrealexf.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")//0 é o primeiro parâmetro do método, 'i' ignora maiusculas e min
+	List<Post> searchTitle(String text);
 
 	List<Post> findByTitleContainingIgnoreCase(String text);//ignoreCase ignora maiusculas e minusculas
 	
